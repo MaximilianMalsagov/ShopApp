22e6a56b0700cc78cf129d426544fa04a65cccf4
Maximilian <maxim.malsagov@mail.ru>
2023-02-27 11:17:41 +0300

    Initial Commit

---
+++ ShopApp/ShopAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopAppApp.swift
+//  ShopApp
+//
+//  Created by Максимилиан Мальсагов on 27.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct ShopAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
